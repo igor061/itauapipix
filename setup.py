7bfdbfdc3e2715c17ab30d0b2558ece980cc3cb5
Igor Miranda <igor061@gmail.com>
2024-11-29 09:37:17 -0300

    correcao no setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name=itauapipix',
+    name='itauapipix',
     version='0.4',
     packages=find_packages(),
     #package_data={'itauapipix': ['server_certs/*.cer']},
